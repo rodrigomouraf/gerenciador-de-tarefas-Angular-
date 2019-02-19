04ac33431fdc2a8a943534ec8eac747fce6b509a
Rodrigo Moura <rodrigomoura_rm@hotmail.com>
2019-02-19 14:33:59 -0300

    Criado Component Editar

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
-//import { EditarTarefaComponent } from './editar';
+import { EditarTarefaComponent } from './editar';
 
 export const TarefaRoutes: Routes = [
 	{ 
@@ -17,8 +17,8 @@ export const TarefaRoutes: Routes = [
 		path: 'tarefas/cadastrar', 
 		component: CadastrarTarefaComponent 
 	},
-	/*{ 
+	{ 
 		path: 'tarefas/editar/:id', 
 		component: EditarTarefaComponent 
-	}*/
+	}
 ];
